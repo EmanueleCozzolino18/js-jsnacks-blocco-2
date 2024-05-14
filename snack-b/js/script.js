0b303f87e72a3ec718fd5acb7854db0ad7d33b42
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-14 21:08:57 +0200

    [UPD] DONE

--- snack-b/js/script.js
+++ snack-b/js/script.js
@@ -13,9 +13,17 @@ function trovaNumero(userSearch, inputArray) {
     // ciclo fin quando l'elemenmto è diverso da undefined
     let i = 0;
     while (inputArray[i] !== undefined) {
-
+        //Se il numero del utente è presente mosta la poszione
+        if (inputArray[i] === userSearch) {
+            return console.log('Il numero è in posizone: ', i);
+            // altrimenti incremento il contatore
+        } else {
+            i++;
+        }
     }
+    // se non entro nel ciclo vuol dire che non é presente
+    return console.log('Errore numero non presente');
 }
 
 // invoco la funzione
-trovaNumero(userElement, myArray);
+trovaNumero(uSearch, inputArray);
