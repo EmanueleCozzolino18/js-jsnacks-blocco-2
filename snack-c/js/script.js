f69c51a1442fad729732d2c166bca8e800a05025
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-14 21:17:16 +0200

    [UPD] snack-c finisehd

--- snack-c/js/script.js
+++ snack-c/js/script.js
@@ -2,3 +2,24 @@
 
 console.log("Script startato correttamente")
 
+const myDesktop = ['computer', 'mouse', 'tastiera', 'cuffie', 'microfono', 'monitor'];
+
+// funzione 
+function stampa(myArrayDesktop) {
+    let string = '';
+    let i = 0;
+    while (myArrayDesktop[i] !== undefined) {
+        if (i == 0) {
+            string += `${myArrayDesktop[i]}`;
+            i++;
+        } else {
+            string += `, ${myArrayDesktop[i]}`;
+            i++;
+        }
+    }
+
+    return string;
+}
+
+
+console.log(stampa(myDesktop));
