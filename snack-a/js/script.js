7497c31b538e065dc6133fffd9e3ce0639000418
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-14 15:48:02 +0200

    [UPD] Created function to count elements

--- snack-a/js/script.js
+++ snack-a/js/script.js
@@ -2,3 +2,16 @@
 
 console.log("Script startato correttamente")
 
+const inputArray = [1, 2, 36, 18, 95, 840, 87, 81, 89, 6, 26];
+
+function contaElementi(input) { // FUNZIONE PER CONTARE ELEMENTI ARRAY
+
+    let i = 0;
+
+    while (input[i] !== undefined) {
+        i++;
+    }
+
+    return i;
+}
+
