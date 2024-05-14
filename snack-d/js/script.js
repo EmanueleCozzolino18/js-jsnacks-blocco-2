c172b02185a0215a8843eb6ad8ac52a7ef1726e2
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-14 21:23:03 +0200

    [UPD]Created frist function

--- snack-d/js/script.js
+++ snack-d/js/script.js
@@ -5,3 +5,20 @@ console.log("Script startato correttamente")
 const myDesktop = ['computer', 'mouse', 'tastiera', 'cuffie', 'microfono', 'monitor'];
 
 
+// assegno ad una variabile length 
+const myDesktopLenght = contaElementi(myDesktop);
+
+
+// funzione per contare gli elementi di un array
+
+function contaElementi(input) {
+
+    let i = 0;
+
+    while (input[i] !== undefined) {
+        i++;
+    }
+
+    return i;
+}
+
