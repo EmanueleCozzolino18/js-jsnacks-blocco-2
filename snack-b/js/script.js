f142af51f28eeeccc43120dc7849202a695679c4
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-14 21:05:49 +0200

    [UPD] Created while cicle

--- snack-b/js/script.js
+++ snack-b/js/script.js
@@ -4,5 +4,18 @@ console.log("Script startato correttamente")
 
 const inputArray = [1, 2, 36, 18, 95, 840, 87, 81, 89, 6, 26];
 
+//Registriamo il numero che l'utente vule cercare
+const uSearch = Number(prompt('Cerca un numero'));
 
+//creiamo la funzione di ricerca per controllare se il numero è presente 
+function trovaNumero(userSearch, inputArray) {
 
+    // ciclo fin quando l'elemenmto è diverso da undefined
+    let i = 0;
+    while (inputArray[i] !== undefined) {
+
+    }
+}
+
+// invoco la funzione
+trovaNumero(userElement, myArray);
